25c05cbe45f07bf7c6790640a4e2e766b2b6f1f9
RaffaelePellegrino <raffaele.pellegrino02@gmail.com>
2024-11-05 16:41:29 +0100

    palidroma

---
+++ esercizi.js
@@ -0,0 +1,17 @@
+const parola=prompt("inserire una parola").toLowerCase();
+
+function palidroma(elemento){
+    let checker = ''
+
+    for(let i= elemento.length-1; i >= 0; i--){
+        checker += elemento[i]
+    }
+    console.log(checker)
+    if(checker === elemento){
+        console.log('la parola è palidroma')
+    }else{
+        console.log('la parola non è palidroma')
+    }
+}
+
+palidroma(parola);
